6212b05e289a1d719f36bce96a3a60963da4e55f
Gabriel Carollo <rimoldi98@gmail.com>
2025-08-25 08:04:16 -0300

    refactored player states and updated movement values

--- lua/src/player.lua
+++ lua/src/player.lua
@@ -2,31 +2,49 @@ player = game_obj:new()
 
 player.transform = transform:new(vector:new(64, 64))
 player.rigidbody = rigidbody:new()
-player.animator = animator:new({animation:new({ 1, 2 }, 2), animation:new({ 3, 4 }, 6)})
+player.animator = animator:new({
+    animation:new({ 1, 2 }, 2), -- idle
+    animation:new({ 3, 4 }, 6), -- move
+    animation:new(5),           -- fall
+})
 
 function player:draw()
-    player.animator:draw(player.transform, player.rigidbody.velocity.x < 0)
+    self.animator:draw(self.transform, self.rigidbody.velocity.x < 0)
 end
 
 function player:update()
-    if btn(0) then
-        player.rigidbody.velocity.x -= .16
-        player.animator:set_state(2)
-    elseif btn(1) then
-        player.rigidbody.velocity.x += .16
-        player.animator:set_state(2)
-    else
-        player.animator:set_state(1)
+    if btn(0) then self:__move(-1)
+    elseif btn(1) then self:__move(1)
+    else self:__idle() end
+
+    if self.rigidbody.velocity.y ~= 0 then
+        self:__fall()
     end
 
-    player.rigidbody:update(player.transform)
+    self.rigidbody:update(self.transform)
 
-    if player.transform.position.y > 120 then
-        player.transform.position.y = 120
-        player.rigidbody.velocity.y = 0
+    if self.transform.position.y > 120 then
+        self.transform.position.y = 120
+        self.rigidbody.velocity.y = 0
     end
 
-    if btn(5) and player.rigidbody.velocity.y == 0 then player.rigidbody.velocity.y -= 8 end
+    if btn(5) and self.rigidbody.velocity.y == 0 then self.rigidbody.velocity.y -= 8 end
+end
+
+function player:__idle()
+    self.rigidbody.drag.x = .5
+    self.animator:set_state(1)
+end
+
+function player:__move(dir)
+    self.rigidbody.velocity.x += .24 * dir
+    self.rigidbody.drag.x = .1
+    self.animator:set_state(2)
+end
+
+function player:__fall()
+    self.rigidbody.drag.x = .075
+    self.animator:set_state(3)
 end
 
 engine:create_obj(player)
